d0506ce66568bf34bc2f6e90eb63b1b977f7e0f0
Rafael Maia <rafaelmaia3002@gmail.com>
2024-04-18 07:59:13 -0300

    implementing the JwtTokenFilter and the JwtConfigurer

--- springboot3_course/rest-with-springboot-erudio/src/main/java/br/com/erudio/security/jwt/JwtTokenProvider.java
+++ springboot3_course/rest-with-springboot-erudio/src/main/java/br/com/erudio/security/jwt/JwtTokenProvider.java
@@ -1,4 +1,4 @@
-package br.com.erudio.securityJwt;
+package br.com.erudio.security.jwt;
 
 import java.util.Base64;
 import java.util.Date;
@@ -29,7 +29,7 @@ public class JwtTokenProvider {
 	@Value("${security.jwt.token.secret-key:secret}")
 	private String secretKey = "secret";
 
-	@Value("${security.jwt.token.expire-lenght:3600000}")
+	@Value("${security.jwt.token.expire-length:3600000}")
 	private long validityInMilliseconds = 3600000; // 1h
 
 	@Autowired
