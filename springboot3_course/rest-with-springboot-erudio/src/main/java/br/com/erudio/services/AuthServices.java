4a45abc5a320fd20a67a65c544f880d327a54fef
Rafael Maia <rafaelmaia3002@gmail.com>
2024-04-25 07:35:49 -0300

    implementing the method refreshToken

--- springboot3_course/rest-with-springboot-erudio/src/main/java/br/com/erudio/services/AuthServices.java
+++ springboot3_course/rest-with-springboot-erudio/src/main/java/br/com/erudio/services/AuthServices.java
@@ -18,33 +18,45 @@ public class AuthServices {
 
 	@Autowired
 	private AuthenticationManager authenticationManager;
-	
+
 	@Autowired
 	private JwtTokenProvider tokenProvider;
-	
+
 	@Autowired
 	private UserRepository repository;
-	
+
 	@SuppressWarnings("rawtypes")
 	public ResponseEntity signin(AccountCredentialsVO data) {
 		try {
 			var username = data.getUsername();
 			var password = data.getPassword();
-			authenticationManager.authenticate(
-					new UsernamePasswordAuthenticationToken(username, password));
-			
+			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+
 			var user = repository.findByUsername(username);
-			
+
 			var tokenResponse = new TokenVO();
 			if (user != null) {
 				tokenResponse = tokenProvider.createAccessToken(username, user.getRoles());
 			} else {
 				throw new UsernameNotFoundException("Username " + username + " not found!");
 			}
-			
+
 			return ResponseEntity.ok(tokenResponse);
 		} catch (Exception e) {
 			throw new BadCredentialsException("Invalid username/password supplied!");
 		}
+	}
+
+	@SuppressWarnings("rawtypes")
+	public ResponseEntity refreshToken(String username, String refreshToken) {
+		var user = repository.findByUsername(username);
+
+		var tokenResponse = new TokenVO();
+		if (user != null) {
+			tokenResponse = tokenProvider.refreshToken(refreshToken);
+		} else {
+			throw new UsernameNotFoundException("Username " + username + " not found!");
+		}
+		return ResponseEntity.ok(tokenResponse);
 	}
 }
