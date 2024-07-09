0068baa829fc44abdb74439b6749af1e122af490
Rafael Maia <rafaelmaia3002@gmail.com>
2024-07-09 20:46:58 -0300

    created FileController for uploading and downloading files

---
+++ springboot3_course/rest-with-springboot-erudio/src/main/java/br/com/erudio/controllers/FileController.java
@@ -0,0 +1,39 @@
+package br.com.erudio.controllers;
+
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import br.com.erudio.data.vo.v1.UploadFileResponseVO;
+import br.com.erudio.services.FileStorageService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "File Endpoint")
+@RestController
+@RequestMapping("/api/file/v1")
+public class FileController {
+
+	private Logger logger = Logger.getLogger(FileController.class.getName());
+	
+	@Autowired
+	private FileStorageService service;
+	
+	@PostMapping("/uploadFile")
+	public UploadFileResponseVO uploadFile(@RequestParam("file") MultipartFile file) {
+		logger.info("Storing file to disk");
+		
+		var filename = service.storeFile(file);
+		String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+				.path("/api/file/v1/downloadFile/")
+				.path(filename)
+				.toUriString();
+		
+		return new UploadFileResponseVO(filename, fileDownloadUri, file.getContentType(), file.getSize());
+	}
+}
