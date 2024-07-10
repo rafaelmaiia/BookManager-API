5c0610552c284037554d09078854b183e5493c77
Rafael Maia <rafaelmaia3002@gmail.com>
2024-07-10 17:13:58 -0300

    Implemented method to upload multiple Files

--- springboot3_course/rest-with-springboot-erudio/src/main/java/br/com/erudio/controllers/FileController.java
+++ springboot3_course/rest-with-springboot-erudio/src/main/java/br/com/erudio/controllers/FileController.java
@@ -1,6 +1,9 @@
 package br.com.erudio.controllers;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,5 +38,15 @@ public class FileController {
 				.toUriString();
 		
 		return new UploadFileResponseVO(filename, fileDownloadUri, file.getContentType(), file.getSize());
+	}
+	
+	@PostMapping("/uploadMultipleFiles")
+	public List<UploadFileResponseVO> uploadMultipleFiles(@RequestParam("files") MultipartFile[] files) {
+		logger.info("Storing files to disk");
+		
+		return Arrays.asList(files)
+				.stream()
+				.map(file -> uploadFile(file))
+				.collect(Collectors.toList());
 	}
 }
