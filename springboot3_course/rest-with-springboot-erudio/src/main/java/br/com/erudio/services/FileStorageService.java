3980eaf0720cece721e188125302973a2420aec8
Rafael Maia <rafaelmaia3002@gmail.com>
2024-07-10 17:31:34 -0300

    Implemented method in FileStorageService for providing file downloads

--- springboot3_course/rest-with-springboot-erudio/src/main/java/br/com/erudio/services/FileStorageService.java
+++ springboot3_course/rest-with-springboot-erudio/src/main/java/br/com/erudio/services/FileStorageService.java
@@ -6,12 +6,15 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import br.com.erudio.config.FileStorageConfig;
 import br.com.erudio.exceptions.FileStorageException;
+import br.com.erudio.exceptions.MyFileNotFoundException;
 
 @Service
 public class FileStorageService {
@@ -45,6 +48,17 @@ public class FileStorageService {
 			return fileName;
 		} catch (Exception e) {
 			throw new FileStorageException("Could not store file " + fileName + ". Please try again!", e);
+		}
+	}
+	
+	public Resource loadFileAsResource(String fileName) {
+		try {
+			Path filePath = this.fileStorageLocation.resolve(fileName).normalize();
+			Resource resource = new UrlResource(filePath.toUri());
+			if (resource.exists()) return resource;	
+			else throw new MyFileNotFoundException("File not found");
+		} catch (Exception e) {
+			throw new MyFileNotFoundException("File not found" + fileName, e);
 		}
 	}
 	
