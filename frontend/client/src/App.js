93673124a7ebc8082a16f71d7d37e5719ec41cc9
Rafael Maia <rafaelmaia3002@gmail.com>
2024-11-09 18:20:19 -0300

    feat: start login implementation in React

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -1,20 +1,11 @@
 import React, {useState} from 'react';
 
-import Header from './Header';
+import './global.css';
+
+import Login from './pages/Login';
 
 export default function App() {
-  const [counter, setCounter] = useState(0);
-  
-  function increment() {
-    setCounter(counter + 1);
-  }
-
   return(
-    <div>
-      <Header>
-        Counter: {counter}
-      </Header>
-      <button onClick={increment}>Add</button>
-    </div>
+    <Login/>
   );
 }
