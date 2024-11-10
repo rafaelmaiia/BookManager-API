7523d0545a88f242e73b0b9dd5a67ddfc24a88b2
Rafael Maia <106180433+rafaelmaiia@users.noreply.github.com>
2024-11-10 16:49:38 -0300

    feat: integrate login screen with Spring API

--- frontend/client/src/pages/Login/index.js
+++ frontend/client/src/pages/Login/index.js
@@ -1,18 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { useNavigate } from 'react-router-dom';
 import './styles.css';
+
+import api from '../../services/api'
 
 import logoImage from '../../assets/logo.svg';
 import padlock from '../../assets/padlock.png';
 
 export default function Login() {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const navigate = useNavigate();
+
+    async function login(e){
+        e.preventDefault();
+        
+        const data = {
+            username,
+            password,
+        };
+
+        try {
+            const response = await api.post('auth/signin', data);
+
+            localStorage.setItem('username', username);
+            localStorage.setItem('accessToken', response.data.token);
+
+            navigate('/books');
+        } catch (err) {
+            alert('Login failed! Try agains!')
+        }
+    };
+
     return (
         <div className='login-container'>
             <section className='form'>
                 <img src={logoImage} alt="Erudio Logo" />
-                <form>
+                <form onSubmit={login}>
                     <h1>Access your Account</h1>
-                    <input placeholder="Username" />
-                    <input type="password" placeholder="Password" />
+                    <input 
+                        placeholder="Username"
+                        value={username}
+                        onChange={e => setUsername(e.target.value)}
+                    />
+                    <input 
+                        type="password" placeholder="Password"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                    />
 
                     <button className='button' type='submit'>Login</button>
                 </form>
