cf8286879a767406fe6061df35d59a64ca293d65
Rafael Maia <106180433+rafaelmaiia@users.noreply.github.com>
2024-11-10 19:20:16 -0300

    feat: integrate delete and logout functionalities with API

--- frontend/client/src/pages/Books/index.js
+++ frontend/client/src/pages/Books/index.js
@@ -16,10 +16,34 @@ export default function Books() {
 
     const navigate = useNavigate();
 
+    async function logout() {
+        localStorage.clear();
+        navigate('/');
+    }
+
+    async function deleteBook(id) {
+        try {
+            await api.delete(`api/book/v1/${id}`, {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            })
+
+            setBooks(books.filter(book => book.id !== id))
+        } catch (err) {
+            alert('Delete failed! Try again.')
+        }
+    }
+
     useEffect(() => {
         api.get('api/book/v1', {
             headers: {
                 Authorization: `Bearer ${accessToken}`
+            },
+            params: {
+                page: 1,
+                limit: 4,
+                direction: 'asc'
             }
         }).then(response => {
             setBooks(response.data._embedded.bookVOList)
@@ -32,7 +56,7 @@ export default function Books() {
                 <img src={logoImage} alt="Erudio" />
                 <span>Welcome, <strong>{username.toUpperCase()}</strong>!</span>
                 <Link className='button' to="/book/new">Add New Book</Link>
-                <button type='button'>
+                <button onClick={logout} type='button'>
                     <FiPower size={18} color='#251FC5' />
                 </button>
             </header>
@@ -40,7 +64,7 @@ export default function Books() {
             <h1>Registered Books</h1>
             <ul>
                 {books.map(book => (
-                    <li>
+                    <li key={book.id}>
                     <strong>Title:</strong>
                     <p>{book.title}</p>
                     <strong>Author:</strong>
@@ -53,7 +77,7 @@ export default function Books() {
                     <button type='button'>
                         <FiEdit size={20} color='#251FC5' />
                     </button>
-                    <button type='button'>
+                    <button onClick={() => deleteBook(book.id)} type='button'>
                         <FiTrash2 size={20} color='#251FC5' />
                     </button>
                 </li>
