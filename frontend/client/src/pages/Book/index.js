a68d15c31287e48cf9756e3f3fd2b7e5dea83ef1
Rafael Maia <106180433+rafaelmaiia@users.noreply.github.com>
2024-11-10 15:20:37 -0300

    implemented header for book screen

--- frontend/client/src/pages/Book/index.js
+++ frontend/client/src/pages/Book/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPower } from 'react-icons/fi';
 
 import './styles.css';
+import logoImage from '../../assets/logo.svg';
 
 export default function Book() {
-    return <h1>Book</h1>
+    return <div>
+        <div className="book-container">
+            <header>
+                <img src={logoImage} alt="Erudio" />
+                <span>Welcome, <strong>Rafael</strong>!</span>
+                <Link className='button' to="book/new">Add New Book</Link>
+                <button type='button'>
+                    <FiPower size={18} color='#251FC5' />
+                </button>
+            </header>
+        </div>
+    </div>
 }
