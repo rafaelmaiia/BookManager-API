70fcfa775f6a96e51b560475e93c6d63d46d6127
Rafael Maia <106180433+rafaelmaiia@users.noreply.github.com>
2024-11-11 18:15:54 -0300

    feat: implement SaveOrUpdate functionality for books

--- frontend/client/src/pages/NewBook/index.js
+++ frontend/client/src/pages/NewBook/index.js
@@ -48,7 +48,7 @@ export default function NewBook() {
 
     const navigate = useNavigate();
 
-    async function createNewBook(e){
+    async function saveOrUpdate(e){
         e.preventDefault();
 
         const data = {
@@ -59,12 +59,21 @@ export default function NewBook() {
         }
 
         try {
-            // Fazendo a requisição para adicionar o novo livro
-            const response = await api.post('api/book/v1', data, {
-                headers: {
-                    Authorization: `Bearer ${accessToken}`
-                }
-            });
+            if (bookId === '0') {
+                await api.post('api/book/v1', data, {
+                    headers: {
+                        Authorization: `Bearer ${accessToken}`
+                    }
+                });
+            } else {
+                data.id = id;
+                // Fazendo a requisição para adicionar o novo livro
+                await api.post('api/book/v1', data, {
+                    headers: {
+                        Authorization: `Bearer ${accessToken}`
+                    }
+                });
+            }            
             navigate('/books');
         } catch (err) {
             alert('Error while recording Book! Try again!')
@@ -75,14 +84,14 @@ export default function NewBook() {
             <div className="content">
                 <section className="form">
                     <img src={logoImage} alt="Erudio" />
-                    <h1>Add New Book</h1>
-                    <p>Enter the book information and click on 'Add'! #### {bookId}</p>
+                    <h1>{bookId === '0' ? 'Add New' : 'Update'} Book</h1>
+                    <p>Enter the book information and click on {bookId === '0' ? "'Add'" : "'Update'"}!</p>
                     <Link className="back-link" to="/books">
                         <FiArrowLeft size={16} color='#251FC5' />
-                        Home
+                        Back to Book
                     </Link>
                 </section>
-                <form onSubmit={createNewBook}>
+                <form onSubmit={saveOrUpdate}>
                     <input
                         placeholder="Title"
                         value={title}
@@ -104,7 +113,7 @@ export default function NewBook() {
                         onChange={e => setPrice(e.target.value)}
                     />
 
-                    <button className='button' type='submit'>Add</button>
+                    <button className='button' type='submit'>{bookId === '0' ? 'Add' : 'Update'}</button>
                 </form>
             </div>
         </div>
